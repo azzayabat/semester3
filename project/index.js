1f2316c3e9473fc28f21f42182064d1e63ca083a
Azzaya Bat-erdene <azzayaa.bat@gmail.com>
2021-05-18 15:14:53 +0800

    Revert "failed"
    This reverts commit 304b5de329a4b103b8ae03a69f952a8d35f9d626.

--- project/index.js
+++ project/index.js
@@ -8,8 +8,6 @@ import MovieApp from './pages/MovieApp';
 import StarFive from './pages/Star';
 import GestureTest from './pages/GestureTest';
 import Homework from './pages/Homework';
-import {Scale} from './src/GestureSystem/Scale';
-import TapNavigation from './src/Navigation/TapNavigation';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => TapNavigation);
+AppRegistry.registerComponent(appName, () => GestureTest);
